648a3a31f24a61d1a18401180f3cfbb57e93b6b1
Matej Malcic <matej.malcic3@gmail.com>
2020-09-06 18:46:08 +0200

    Markup Validation done

---
+++ template/header.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Academy 2020</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css">
+</head>
